5d747dd8a337b3dc8de0903782b2186bf6404ab8
Vladimir <vladimir.papin@gmail.com>
2023-12-21 12:53:48 +0300

    Rewrite data validation logic with DRF

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,12 +1,17 @@
-import json
-import re
-
 from django.http import JsonResponse
 from django.templatetags.static import static
+from phonenumber_field.validators import validate_international_phonenumber
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.serializers import (CharField, ModelSerializer, Serializer,
+                                        ValidationError)
 
 from .models import MakeOrder, Product, ProductOrder
+
+
+class ApplicationSerializer(Serializer):
+    contact_phone = CharField()
+    ticket_type = CharField()
 
 
 def banners_list_api(request):
@@ -61,6 +66,31 @@ def product_list_api(request):
     })
 
 
+class ProductOrderSerializer(ModelSerializer):
+
+    class Meta:
+        model = ProductOrder
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(Serializer):
+    firstname = CharField(label='Имя', max_length=50)
+    lastname = CharField(label='Фамилия', max_length=50)
+    address = CharField(label='Адрес', max_length=50)
+    phonenumber = CharField(
+        label='Контактный телефон',
+        max_length=128)
+    products = ProductOrderSerializer(many=True, allow_empty=False)
+
+    def validate_phonenumber(self, value):
+        """Tracker phone number validation."""
+        try:
+            validate_international_phonenumber(value)
+        except ValidationError:
+            raise ValidationError('Phone number is not valid.')
+        return value
+
+
 @api_view(['POST'])
 def register_order(request):
     try:
@@ -71,101 +101,22 @@ def register_order(request):
             'valueerror': ValueError
         })
 
-    print(f'-=-=-=-=-=-=-=-=\nData: {data}')
-    if 'products' not in data:
-        return Response({
-            'error': 'products: Обязательное поле.'
-        })
-    if not data['products']:
-        return Response({
-            'error': 'products: Это поле не может быть пустым.'
-        })
-
-    if type(data['products']) is not list:
-        return Response({
-            'error': 'products: Ожидался list со значениями.'
-        })
-
-    elif len(data['products']) == 0:
-        return Response({
-            'error': 'products: Этот список не может быть пустым.'
-        })
-
-    if 'firstname' not in data or 'lastname' not in data or 'phonenumber' not in data or 'address' not in data:
-
-        return Response({
-            'firstname, lastname, phonenumber, address: Обязательное поле.'
-        })
-
-    if not data['firstname']:
-        return Response({
-            'firstname: Это поле не может быть пустым.'
-        })
-
-    if not data['lastname']:
-        return Response({
-            'lastname: Это поле не может быть пустым.'
-        })
-
-    if not data['phonenumber']:
-        return Response({
-            'phonenumber: Это поле не может быть пустым.'
-        })
-
-    if not data['address']:
-        return Response({
-            'address: Это поле не может быть пустым.'
-        })
-
-    if type(data['firstname']) is not str:
-        return Response({
-            'error': 'firstname: Not a valid string.'
-        })
-
-    if type(data['lastname']) is not str:
-        return Response({
-            'error': 'lastname: Not a valid string.'
-        })
-
-    if type(data['address']) is not str:
-        return Response({
-            'address: Not a valid string.'
-        })
-
-    if not re.match(r'^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$', data['phonenumber']):
-        return Response({
-            'phonenumber': 'Введен некорректный номер телефона.'
-        })
+    serializer = OrderSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
 
     make_order = MakeOrder.objects.create(
-        first_name=data['firstname'],
-        last_name=data['lastname'],
-        address=data['address'],
-        contact_phone=data['phonenumber']
+        first_name=serializer.validated_data['firstname'],
+        last_name=serializer.validated_data['lastname'],
+        address=serializer.validated_data['address'],
+        contact_phone=serializer.validated_data['phonenumber']
     )
 
-    for product in data['products']:
-        
-        product_id = product['product']
-        try:
-            product = Product.objects.get(id=product_id)
-        except:
-            return Response({
-                f'products: Недопустимый первичный ключ {product_id}'
-            })
+    for product in serializer.validated_data['products']:
 
         ProductOrder.objects.create(
             order=make_order,
-            product= Product.objects.get(id=product['product']),
+            product=product['product'],
             quantity=product['quantity']
         )
 
     return Response({})
-
-# {'products': [{'product': 2, 'quantity': 1},
-#               {'product': 3, 'quantity': 2},
-#               {'product': 6, 'quantity': 1}],
-# 'firstname': 'Владимир',
-# 'lastname': 'Папин',
-# 'phonenumber': '89660955537',
-# 'address': 'ул. Адмиралтейская 51, '}
